5d1693a471e9de458ebaf65f3ffa19dc46a91421
Joshua Crum <joshua.crum@gmail.com>
2019-06-07 19:18:22 +0000

    Updated environment requirements and bin folder.

--- lib/album_review.rb
+++ lib/album_review.rb
@@ -1,6 +1,12 @@
 require "album_review/version"
+require "album_review/albums"
+require "album_review/CLI"
+require "album_review/scraper"
 
-module AlbumReview
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require "Nokogiri"
+require "pry"
+require "open-uri"
+
+require_all "./lib/album_review"
+
+require_relative "./album_review"
