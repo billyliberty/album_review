7834090af99e5fcec4d77eb4e170ce807dd417a4
Joshua Crum <joshua.crum@gmail.com>
2019-06-07 19:52:14 +0000

    Added to scraper class.

--- lib/album_review/scraper.rb
+++ lib/album_review/scraper.rb
@@ -1,3 +1,19 @@
 class AlbumReview::Scraper
   
+  def self.scrape_index_page(index_url)
+    page = Nokogiri::HTML(open(index_url))
+    album_array = []
+    page.css("div.review").each do |album|
+      title =
+      artist =
+      genre =
+      url =
+      review_score =
+      
+      album = {:title => title, :artist => artist, :genre => genre, :url => url, :review_score = review_score}
+      album_array << album
+      end
+    album_array
+   end
+
 end
