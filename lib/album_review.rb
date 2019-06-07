9793caf541f3e6a24460792e9d703eb8b0accf08
Joshua Crum <joshua.crum@gmail.com>
2019-06-07 18:55:57 +0000

    first commit

---
+++ lib/album_review.rb
@@ -0,0 +1,6 @@
+require "album_review/version"
+
+module AlbumReview
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/album_review/version.rb
@@ -0,0 +1,3 @@
+module AlbumReview
+  VERSION = "0.1.0"
+end
