876bb20cb71d55bfd8615f584b5abb5ddbff8704
Joshua Crum <joshua.crum@gmail.com>
2019-06-07 19:38:07 +0000

    Updated CLI class.

--- lib/album_review.rb
+++ lib/album_review.rb
@@ -1,12 +1,12 @@
-require "album_review/version"
-require "album_review/albums"
-require "album_review/CLI"
-require "album_review/scraper"
+require_relative "album_review/version"
+require_relative "album_review/albums"
+require_relative "album_review/CLI"
+require_relative "album_review/scraper"
 
-require "Nokogiri"
-require "pry"
-require "open-uri"
+#require 'pry'
+#require 'nokogiri'
+#require 'open-uri'
 
-require_all "./lib/album_review"
+require "./lib/album_review"
 
 require_relative "./album_review"
--- lib/album_review/CLI.rb
+++ lib/album_review/CLI.rb
@@ -1,4 +1,18 @@
 class AlbumReview::CLI 
   
+  def call
+    puts "Welcome to the best new albums as reviewed by Pitchfork!"
+    list_albums
+    menu
+  end
+  
+  def list_albums
+    puts "Zuu by Denzel Curry"
+  end
+  
+  def menu
+    puts "Enter the number of the album for that album's review score."
+  end
+  
 end
   
