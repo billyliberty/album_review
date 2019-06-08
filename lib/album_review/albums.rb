d64f2969d7a4e0315d832aa8e00382220686d282
Joshua Crum <joshua.crum@gmail.com>
2019-06-08 22:22:47 +0000

    Intitial scraping method.

--- lib/album_review/albums.rb
+++ lib/album_review/albums.rb
@@ -1,21 +1,27 @@
 class AlbumReview::Albums
   
-  attr_accessor :name, :artist, :genre, :review_score, :review_date, :reviewer
-  
-
+  attr_accessor :name, :artist, :genre, :score, :url
   
   def self.all
-    
-    album_1 = self.new 
-    album_1.name = "White Album"
-    album_1.artist = "Beatles"
-    
-    album_2 = self.new 
-    album_2.name = "Black Album"
-    album_2.artist = "Jay-Z"
-    
-    [album_1, album_2]
+    self.scrape_albums
   end
+  
+  def self.scrape_albums
+    albums = []
+    albums << self.scrape_albums
+    albums
+  end
+  
+  def self.scrape_pitchfork
+    page = Nokogiri::HTML(open(https://pitchfork.com/reviews/best/albums/))
+    
+    album=self.new 
+    album.name = page.css("")
+    album.artist
+    
+    album
+  end
+
   
   
   
--- lib/album_review/scraper.rb
+++ lib/album_review/scraper.rb
@@ -1,5 +1,7 @@
 class AlbumReview::Scraper
   
+
+  
   
   
   
