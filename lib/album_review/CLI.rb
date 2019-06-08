c6a056755d1ec958cdf3ce6e3a614a58e3de79b7
Joshua Crum <joshua.crum@gmail.com>
2019-06-08 17:05:50 +0000

    Updtaed CLI class with menu choices.

--- lib/album_review/CLI.rb
+++ lib/album_review/CLI.rb
@@ -11,12 +11,28 @@ class AlbumReview::CLI
   
   def genre_albums
     puts "1. Rap, 2. Rock, 3. Pop, 4. R&B, 5. Experimental"
+    input = gets.strip.downcase
+    case input
+      when "1" 
+        puts "RAP"
+      when "2"
+        puts "ROCK"
+      when "3" 
+        puts "POP"
+      when "4" 
+        puts "R&B"
+      when "6" 
+        puts "EXPERIMENTAL"
+      else ""
+        puts "I don't understand your selection. Please try again."
+        genre_albums
+    end
+    
   end
   
   def goodbye
     puts "Goodbye!"
   end
-    
   
   def menu
     puts "Please enter: 'list' for a list of albums or 'genre' for a list of genres. Enter 'quit' to exit."
@@ -30,7 +46,6 @@ class AlbumReview::CLI
         goodbye
       end
     end
-  
 end
   
 
--- lib/album_review/genre.rb
+++ lib/album_review/genre.rb
@@ -2,4 +2,12 @@ class AlbumReview::Genre
   
   attr_accessor :albums
   
+  def initialize()
+    @albums = []
+  end
+  
+  def add_albums
+    @albums << album 
+  end
+  
 end
