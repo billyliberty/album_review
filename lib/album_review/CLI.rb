395c599ec1bca87cc39cba420c03f4617859c302
Joshua Crum <joshua.crum@gmail.com>
2019-06-08 18:28:47 +0000

    Updated CLI menu choices.

--- lib/album_review/CLI.rb
+++ lib/album_review/CLI.rb
@@ -3,9 +3,8 @@ class AlbumReview::CLI
   def call
     puts "Welcome to the best new albums as reviewed by Pitchfork!"
     puts "--------------------------"
-    puts "Every review collated for your eyes so you can treat your ears!"
+    puts "Every review collated for your eyes to read so you can treat your ears!"
     puts "--------------------------"
-    puts "To access reviews:"
     menu
   end
   
@@ -14,7 +13,7 @@ class AlbumReview::CLI
   end
   
   def genre_albums
-    puts "1. Rap, 2. Rock, 3. Pop, 4. R&B, 5. Experimental"
+    puts "1. Rap, 2. Rock, 3. Pop, 4. R&B, 5. Experimental (or 'menu' to return to the main menu)"
     input = gets.strip.downcase
     case input
       when "1" 
@@ -27,6 +26,8 @@ class AlbumReview::CLI
         puts "R&B"
       when "6" 
         puts "EXPERIMENTAL"
+      when "menu"
+        menu
       when "quit"
         goodbye
       else ""
@@ -41,6 +42,7 @@ class AlbumReview::CLI
   end
   
   def menu
+    puts "To access reviews:"
     puts "Please enter 'list' for a list of albums or 'genre' for a list of genres. 
     (Enter 'quit' to exit at any point.)"
     input = gets.strip.downcase
--- lib/album_review/scraper.rb
+++ lib/album_review/scraper.rb
@@ -1,4 +1,6 @@
 class AlbumReview::Scraper
   
   
+  
+  
 end
