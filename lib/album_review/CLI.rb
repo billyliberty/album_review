26947db803de4efdfc88237537bc30f7a6bfe642
Joshua Crum <joshua.crum@gmail.com>
2019-06-09 23:07:45 +0000

    Update to CLI

--- lib/album_review/CLI.rb
+++ lib/album_review/CLI.rb
@@ -1,15 +1,25 @@
 class AlbumReview::CLI 
   
+  attr_accessor :albums
+  
   def call
     puts "Welcome to the best new albums as reviewed by Pitchfork!"
     puts "--------------------------"
     puts "Every review collated for your eyes to read so you can treat your ears!"
     puts "--------------------------"
+    AlbumReview::Scraper.scrape_albums
+    albums
     album_list
+    puts "For the review score of an album, please enter a number from 1-10."
+    get_albums
   end
   
+  def albums 
+    @albums = AlbumReview::Albums.all
+  end
+    
   def album_list
-    AlbumReview::Albums.all.each do |index, album|
+    @albums.each.with_index(1) do |album, index|
       puts "#{index}. #{album.title} by #{album.artist}"
     end
   end
